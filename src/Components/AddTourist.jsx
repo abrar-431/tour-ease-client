3bca185b530975d62ef36098d9a4b343fd885a78
unknown <abrarh1520444@gmail.com>
2024-04-28 23:52:06 +0600

    add tourist spot added

--- src/Components/AddTourist.jsx
+++ src/Components/AddTourist.jsx
@@ -1,12 +1,46 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddTourist = () => {
+    const handleAddTourist = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const spot = form.spot.value;
+        const country = form.country.value;
+        const location = form.location.value;
+        const cost = form.cost.value;
+        const seasonality = form.seasonality.value;
+        const time = form.time.value;
+        const visitors = form.visitors.value;
+        const image = form.image.value;
+        const description = form.description.value;
+
+        const touristSpot = {name, email, spot, country, location, cost, seasonality, time, visitors, image, description};
+        fetch('http://localhost:5000/spots', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(touristSpot),
+        })
+        .then(res => res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.insertedId){
+                toast('Tourist Spot added successfully')
+            }
+            form.reset();
+        })
+    }
     return (
         <div>
             <h2 className="text-2xl font-bold text-center mt-10">Add Tourist Spots</h2>
             <p className="text-center w-2/3 mx-auto my-5">Use this Form to Share Your Must-See Tourist Spots and Inspire Fellow Travelers on Their Next Adventure!</p>
-            <div className="flex justify-between">
+            <div className="flex gap-6">
                 <div className="w-1/3">
                     <Carousel>
                         <div>
@@ -27,29 +61,64 @@ const AddTourist = () => {
                         </div>
                     </Carousel>
                 </div>
-                <form className="bg-gray-300 p-10 rounded-xl">
-                    <div className="flex gap-10 mb-10">
-                        <label>Name</label><input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
+                <form onSubmit={handleAddTourist} className="bg-slate-700 p-10 rounded-xl">
+                    <div className="flex gap-4 mb-10">
+                        <div className="w-1/3">
+                            <label className="font-medium">User Name</label>
+                            <input type="text" placeholder="User Name" name="name" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/3">
+                            <label className="font-medium">User Email</label>
+                            <input type="text" placeholder="User Email" name="email" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/3">
+                            <label className="font-medium">Tourist Spot Name</label>
+                            <input type="text" placeholder="Tourist Spot Name" name="spot" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+
                     </div>
-                    <div className="flex gap-10 mb-10">
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
+                    <div className="flex gap-4 mb-10">
+                        <div className="w-1/3">
+                            <label className="font-medium">Country Name</label>
+                            <input type="text" placeholder="Country Name" name="country" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/3">
+                            <label className="font-medium">Location</label>
+                            <input type="text" placeholder="Location" name="location" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/3">
+                            <label className="font-medium">Average Cost</label>
+                            <input type="text" placeholder="Average Cost" name="cost" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
                     </div>
-                    <div className="flex gap-10 mb-10">
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
+                    <div className="flex gap-4 mb-10">
+                        <div className="w-1/3">
+                            <label className="font-medium">Seasonality</label>
+                            <input type="text" placeholder="Seasonality" name="seasonality" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/3">
+                            <label className="font-medium">Travel Time</label>
+                            <input type="text" placeholder="Travel Time" name="time" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/3">
+                            <label className="font-medium">Total Visitors Per Year</label>
+                            <input type="text" placeholder="Total Visitors Per Year" name="visitors" className="input input-bordered w-full max-w-xs bg-gray-200 mt-2" />
+                        </div>
                     </div>
-                    <div className="flex gap-10 mb-10">
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs  bg-gray-200" />
+                    <div className="flex gap-6 mb-10">
+                        <div className="w-1/2">
+                            <label className="font-medium">Image URL</label>
+                            <input type="text" placeholder="Image URL" name="image" className="input input-bordered w-full bg-gray-200 mt-2" />
+                        </div>
+                        <div className="w-1/2">
+                            <label className="font-medium">Short Description</label>
+                            <input type="text" placeholder="Short Description" name="description" className="input input-bordered w-full bg-gray-200 mt-2" />
+                        </div>
                     </div>
+                    <button className="btn w-full">Add</button>
                 </form>
             </div>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
--- src/Components/AllTourist.jsx
+++ src/Components/AllTourist.jsx
@@ -3,7 +3,15 @@
 const AllTourist = () => {
     return (
         <div>
-            <h2>ALl Tourist Spots</h2>
+            <div className="mt-6">
+            <h2 className="text-2xl font-bold text-center">European Escapes: Unforgettable Tourist Spots</h2>
+            <p className="w-4/5 text-center mx-auto mt-3">Embark on a virtual tour through Europe's most iconic destinations. From the romance of Paris to the ancient wonders of Rome, explore breathtaking landmarks and hidden gems that define the essence of European travel.</p>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-6 mt-5">
+            {
+                
+            }
+        </div>
+        </div>
         </div>
     );
 };
