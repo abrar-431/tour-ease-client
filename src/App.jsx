f94483feb4ad48fff7d0ab137b4d2b323894bb06
unknown <abrarh1520444@gmail.com>
2024-04-28 11:56:48 +0600

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold'>Tourism Manager</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgfZWIMxZsP3kRqKsqoC-eT7K1oyH2otc",
+  authDomain: "tourism-manager-6b4fe.firebaseapp.com",
+  projectId: "tourism-manager-6b4fe",
+  storageBucket: "tourism-manager-6b4fe.appspot.com",
+  messagingSenderId: "668752110184",
+  appId: "1:668752110184:web:ec3a2d87270c5dbb7891dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
